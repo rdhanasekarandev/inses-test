b155bb747ea3ed6b7686a96f4d252f81d53c5b96
vananjeeva <vananjeeva@gmail.com>
2021-01-23 01:41:57 +0530

    Initial commit

---
+++ src/com/inses/InsesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inses;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/inses/InsesApiApplication.java
@@ -0,0 +1,13 @@
+package com.inses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsesApiApplication.class, args);
+	}
+
+}
