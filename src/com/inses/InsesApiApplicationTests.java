2a2b77acf53ee387eb943a53e09263874925cdc8
vananjeeva <vananjeeva@gmail.com>
2021-01-23 01:56:19 +0530

    initin

--- src/com/inses/InsesApiApplicationTests.java
+++ src/com/inses/InsesApiApplicationTests.java
@@ -1,13 +1,9 @@
 package com.inses;
 
-import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InsesApiApplicationTests {
 
-	@Test
-	void contextLoads(){
-	}
 
 }
